3466975c87e8ca74729eb715c953d2ebd09e1c1b
Isaiah Gayfield <igayfield7@gmail.com>
2017-07-12 07:29:27 -0700

    Add primes.json
    Primes.json will be used for storing the prime numbers.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  var contents = fs.readFileSync('primes.json')
+  var primes = JSON.parse(contents)
+  res.render('index', { title: 'Express', number: primes.now, lastnumber: primes.last })
 });
 
 module.exports = router;
